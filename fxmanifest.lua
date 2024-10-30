725da17ba4ae1350fb06f2775272086cdfae9fb4
JimmySackss <134826848+JimmySackss@users.noreply.github.com>
2024-10-29 22:21:27 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'A jimmysackss joint'
+description 'A fivem door pop script for LEO RP using emergency vehicles.'
+version '1.0.0'
+
+client_script 'client.lua'
